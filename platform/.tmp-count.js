0f7662d1556e71ad1d9b1f634d9c49150a120241
mjpt1 <mjpt1@users.noreply.github.com>
2026-07-26 14:25:46 +0330

    Remove temporary count scripts from platform.

--- platform/.tmp-count.js
+++
@@ -1,3 +0,0 @@
-﻿const {PrismaClient}=require("@prisma/client");
-const p=new PrismaClient();
-p.user.count().then(c=>{console.log("user_count",c); return p.$disconnect();}).catch(e=>{console.error("ERR",e); process.exit(1);});
--- platform/.tmp-count2.js
+++
@@ -1,3 +0,0 @@
-﻿const {PrismaClient}=require("@prisma/client");
-const p=new PrismaClient();
-Promise.all([p.user.count(), p.patient.count(), p.organization.count()]).then(([u,pa,o])=>{console.log({users:u,patients:pa,orgs:o}); return p.$disconnect();}).catch(e=>{console.error(e); process.exit(1);});
